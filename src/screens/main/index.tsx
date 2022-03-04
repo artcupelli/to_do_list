2230d0f1e3d29c2168bdcf29288a9a141539ac1b
Artur Cupelli <artur.cupelli@usp.br>
2022-03-04 15:55:38 -0300

    TDL-4 Cria estrutura tela inicial

---
+++ src/screens/main/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './main.module.scss';
+
+
+const Main: React.FC = () => {
+
+  return (
+    <div className={styles['container']}>
+      Main Screen
+    </div>
+  );
+}
+
+export default Main;
